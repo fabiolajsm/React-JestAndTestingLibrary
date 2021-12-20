1b2ebe205d06da39640bf04799745ac16a0d7d4a
Fabiola <you@example.com>
2021-12-20 10:39:16 -0300

    Section 5 (check hover)

--- sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Popover from 'react-bootstrap/Popover'
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 export default function SummaryForm({ setOrderPhase }) {
-  const [checked, setChecked] = useState(false)
+  const [tcChecked, setTcChecked] = useState(false);
 
-  // function handleSubmit(event) {
-  //   event.preventDefault()
+  function handleSubmit(event) {
+    event.preventDefault();
 
-  //   // pass along to the next phase.
-  //   // The next page will handle submitting order from context.
-  //   setOrderPhase('completed')
-  // }
+    // pass along to the next phase.
+    // The next page will handle submitting order from context.
+    setOrderPhase('completed');
+  }
 
   const popover = (
     <Popover id="termsandconditions-popover">
       <Popover.Content>No ice cream will actually be delivered</Popover.Content>
     </Popover>
-  )
+  );
 
   const checkboxLabel = (
     <span>
@@ -28,20 +28,21 @@ export default function SummaryForm({ setOrderPhase }) {
         <span style={{ color: 'blue' }}> Terms and Conditions</span>
       </OverlayTrigger>
     </span>
-  )
+  );
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="terms-and-conditions">
         <Form.Check
           type="checkbox"
-          checked={checked}
-          onChange={(e) => setChecked(e.target.checked)}
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
           label={checkboxLabel}
         />
       </Form.Group>
-      <Button variant="primary" type="submit" disabled={!checked}>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
         Confirm order
       </Button>
     </Form>
-  )
+  );
 }
--- sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import SummaryForm from '../SummaryForm'
 import userEvent from '@testing-library/user-event'
 
@@ -27,24 +31,25 @@ test('After click, checkbox is checked and button is enabled', () => {
   expect(buttonConfirm).toBeDisabled()
 })
 
-test('Popover responds to hover', () => {
+test('Popover responds to hover', async () => {
   render(<SummaryForm />)
-  // popover stars hidden
+  // popover stars out hidden
   const nullPopover = screen.queryByText(
     /no ice cream will actually be delivered/i,
   )
   expect(nullPopover).not.toBeInTheDocument()
+
   // popover appears upon mouseover of checkbox label
   const termsAndConditions = screen.getByText(/terms and conditions/i)
   userEvent.hover(termsAndConditions) // aqui falla whaaaaat???
 
   const popover = screen.getByText(/no ice cream will actually be delivered/i) // this will throw an error but
   expect(popover).toBeInTheDocument() // put this is best practice bc makes the code more readable.
- 
+
   // popover disappears when mouse out
   userEvent.unhover(termsAndConditions)
-  const nullPopoverAgain = screen.queryByText(
-    /no ice cream will actually be delivered/i,
-  )
-  expect(nullPopoverAgain).not.toBeInTheDocument()
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i),
+  ) 
+  // expect(waitForElementToBeRemoved).not.toBeInTheDocument() => No anymore
 })
