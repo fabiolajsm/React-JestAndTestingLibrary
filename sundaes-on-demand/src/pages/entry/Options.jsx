4ff0a52b7bfe73fed8d212b5f2b392a1944f38e5
Fabiola <you@example.com>
2021-12-27 17:40:50 -0300

    Test and code alert for server errors

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -3,21 +3,24 @@ import { useEffect, useState } from 'react'
 import ScoopOption from './ScoopOption'
 import Row from 'react-bootstrap/Row'
 import ToppingOption from './ToppingOption'
+import AlertBanner from '../common/AlertBanner'
 
 export default function Options({ optionType }) {
   // optionType is "scoops" or "toppings"
   const [items, setItems] = useState([])
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     axios
       .get(`http:localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((err) => {
-        // TODO: handler response error later
-      })
+      .catch((err) => setError(true))
   }, [optionType])
 
-  // TODO: replace null with ToppingOption when available
+  if (error) {
+    return <AlertBanner />
+  }
+
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption
 
   const optionItems = items.map((item) => (
