baaff48fa3e6b3a47f00922367f2808f81ce8b9c
Fabiola <you@example.com>
2021-12-28 17:28:33 -0300

    Topping subtotal test and code

--- sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
@@ -26,3 +26,30 @@ test('update scoop subtotal when scoops change', async () => {
   userEvent.type(chocolateInput, '2')
   expect(scoopsSubtotal).toHaveTextContent('6.00')
 })
+
+test('update toppings subtotal when toppings change', async () => {
+  render(<Options optionType="toppings" />, { wrapper: OrderDetailsProvider })
+  // make sure total starts out $0.00
+  const toppingSubtotal = screen.getByText('Toppings total: $', {
+    exact: false,
+  })
+  expect(toppingSubtotal).toHaveTextContent('0.00')
+
+  // update cherrie topping to 1 and check the subtotal
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  })
+  userEvent.click(cherriesCheckbox)
+  expect(toppingSubtotal).toHaveTextContent('1.50')
+
+  // add hot fudge and check subtotal
+  const hotFudgeCheckbox = await screen.findByRole('checkbox', {
+    name: 'Hot fudge',
+  })
+  userEvent.click(hotFudgeCheckbox)
+  expect(toppingSubtotal).toHaveTextContent('3.00')
+
+  // remove hot fudge and check subtotal
+  userEvent.click(hotFudgeCheckbox)
+  expect(toppingSubtotal).toHaveTextContent('1.50')
+})
