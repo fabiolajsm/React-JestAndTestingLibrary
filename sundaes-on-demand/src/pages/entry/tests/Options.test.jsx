0f7a4bc176787a3513378c74ba15b60db6198d09
Fabiola <you@example.com>
2021-12-28 16:16:41 -0300

    Test and code context for scoops subtotal

--- sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react' // better render with the global but in this cases is just to see it
+import { OrderDetailsProvider } from '../../../contexts/OrderDetails'
 import Options from '../Options'
 
 test('display image for each scoop option from server (mocks)', async () => {
-  render(<Options optionType="scoops" />)
+  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider })
   // find images
   const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i })
   expect(scoopImages).toHaveLength(2)
@@ -13,7 +14,7 @@ test('display image for each scoop option from server (mocks)', async () => {
 })
 
 test('display image for each topping option from server', async () => {
-  render(<Options optionType="toppings" />)
+  render(<Options optionType="toppings" />, { wrapper: OrderDetailsProvider })
   const toppingImages = await screen.findAllByRole('img', { name: /topping$/i })
   expect(toppingImages).toHaveLength(3)
 
