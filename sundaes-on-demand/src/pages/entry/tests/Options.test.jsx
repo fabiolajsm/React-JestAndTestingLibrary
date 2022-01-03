00576ea56085f5fb79fd2b33ac39d2478c2d88c7
Fabiola <you@example.com>
2022-01-03 12:52:47 -0300

    Test dont update the subtotal with an invalid input

--- sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react' // better render with the global but in this cases is just to see it
+import userEvent from '@testing-library/user-event'
 import { OrderDetailsProvider } from '../../../contexts/OrderDetails'
 import Options from '../Options'
 
@@ -24,4 +25,19 @@ test('display image for each topping option from server', async () => {
     'M&Ms topping',
     'Hot fudge topping',
   ])
+})
+
+test('Dont update the subtotal when scoops input is invalid', async () => {
+  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider })
+
+  // expect button to be enabled after adding scoop
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  })
+  userEvent.clear(vanillaInput)
+  userEvent.type(vanillaInput, '-1')
+
+  // make sure subtotals hasnt updated
+  const scoopsSubtotal = screen.getByText('Scoops total: $0.00')
+  expect(scoopsSubtotal).toBeInTheDocument()
 })
